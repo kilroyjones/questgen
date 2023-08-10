0ea7005189247115a7718159c2676d851923d145
kilroy <tsbach@gmail.com>
2023-08-10 11:12:43 -0700

    fixing

--- frontend_old/src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import Navbar from '$lib/navigation/Navbar.svelte';
-	import '../app.css';
-</script>
-
-<Navbar />
-<slot />
--- frontend_old/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit(), wasm(), topLevelAwait()]
-});
